fdf65fc4526d09c9c9018b4798dfa7545722816d
Warren Sadler <warrendsadler@gmail.com>
2020-05-05 00:50:34 -0500

    feat: :construction: create temp test and main script
    create initial components for testing and application runtime

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+/**
+ * @module src/main.ts
+ *
+ * Main.ts operates as the entry point to our application.
+ */
+
+console.log('Hello World!');
---
+++ tests/unit/sample.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * ! This is a placeholder test
+ */
+
+describe('Basic Arithmetic', () => {
+    it('works', () => {
+        expect(1 + 1).toBe(2);
+    });
+});
