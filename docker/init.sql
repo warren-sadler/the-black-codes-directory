389f13e1a49b493bfd36883cdf1cdffe73041163
Warren Sadler <warrendsadler@gmail.com>
2020-05-08 21:48:27 -0500

    feat: :card_file_box: configure database containerization

---
+++ docker/init.sql
@@ -0,0 +1,4 @@
+CREATE OR REPLACE USER the_black_codes_admin;
+
+CREATE OR REPLACE DATABASE the_black_codes_directory_dev;
+GRANT ALL PRIVILEGES ON DATABASE the_black_codes_directory_dev TO the_black_codes_admin;
